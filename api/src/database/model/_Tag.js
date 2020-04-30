b67d11cd8ccdfe6462a15839567423f28a50daf8
eddie <properllerbucher@gmail.com>
2020-04-30 10:12:51 +0200

    fixed missing boolean in Tag model

--- api/src/database/model/_Tag.js
+++ api/src/database/model/_Tag.js
@@ -3,7 +3,7 @@ const { Schema, model, Types } = require("mongoose");
 // Define schema that has 4 variables: tag, userId, and username. We also assign
 const tagSchema = new Schema({
   tag: { type: Number, required: true },
-  userId: { type: Types.ObjectId, required: true, unique },
+  userId: { type: Types.ObjectId, required: true, unique: true },
   username: { type: String, required: true },
 });
 
